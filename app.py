549479bd4907c404c5c851c25edbb78b801595ba
Yatendra <yatendra1999luffy@gmail.com>
2017-11-04 22:04:55 +0530

    a

--- app.py
+++ app.py
@@ -21,37 +21,51 @@ mysql = MySQL(app)
 
 @app.route('/', methods=['GET', 'POST'])
 def homepage():
-    if request.method == 'POST':
-        gid = request.form['gid']
-        password_candid = request.form['password']
-
-        cur = mysql.connection.cursor()
-
-        result = cur.execute(" SELECT * FROM groups  WHERE gid = %s", [gid])
-
-        if result == 0:
-            flash('Group id not found ', 'danger')
-            return render_template('login.html')
-        if result > 0:
-            data = cur.fetchone()
-            password = data['password']
-
-        if sha256_crypt.verify(password_candid, password):
-
-            session['logged_in'] = True
-            session['gid'] = gid
-            session['level'] = '0'
-            flash('Login successful', 'success')
+    if 'logged_in' in session:
+        if session['level'] == '0':
             return redirect(url_for('l1'))
+        elif session['level'] == '1':
+            return redirect(url_for('l2'))
+        elif session['level'] == '2':
+            return redirect(url_for('l3'))
+        elif session['level'] == '3':
+            return redirect(url_for('l4'))
+        elif session['level'] == '4':
+            return redirect(url_for('l5'))
+        elif session['level'] == '5':
+            return redirect(url_for('lboard'))
         else:
-            flash('Wrong username/password', 'danger')
-            return redirect(url_for('homepage'))
-        cur.close()
+            return redirect(url_for('logout'))
 
-    return render_template('homepage.html')
+    else:
+        if request.method == 'POST':
+            username = request.form['username']
+            password_candid = request.form['password']
+            cur = mysql.connection.cursor()
+            result = cur.execute(" SELECT * FROM users WHERE user = %s", [username])
+            if result == 0:
+                flash('Username not found ', 'danger')
+                return render_template('homepage.html')
+            if result > 0:
+                data = cur.fetchone()
+                password = data['password']
+
+            if password == password_candid:
+
+                session['logged_in'] = True
+                session['username'] = username
+                session['level'] = '0'
+                flash('Login successful', 'success')
+                return redirect(url_for('l1'))
+            else:
+                flash('Wrong username/password', 'danger')
+                return redirect(url_for('homepage'))
+            cur.close()
+
+        return render_template('homepage.html')
 
 
-@app.route('/level1',methods=['GET', 'POST'])
+@app.route('/level1', methods=['GET', 'POST'])
 def l1():
     if 'logged_in' in session:
         if request.method == 'POST':
@@ -86,44 +100,88 @@ def l1():
         return redirect(url_for('homepage'))
 
 
-@app.route('/level2', methods=['GET', 'POST'])
+@app.route('/potato_io/shots/count/enterhere', methods=['GET', 'POST'])
 def l2():
     if 'logged_in' in session:
         if session['level'] >= '1':
-            if request.method == 'POST':
-                if request.form['password'] == '':
-                    session['level'] = '2'
-                    return render_template('level2.html')
-                else:
-                    flash('wrong password', 'danger')
-                    return redirect(url_for('l1'))
-            else:
-                return render_template('level2.html')
+            return render_template('level2.html')
         else:
             return redirect(request.referrer)
     else:
-        flash('User must login first', 'danger')
-        return redirect(url_for('homepage'))
+        flash('user must login first', 'danger')
+        return redirect(url_for(homepage))
 
+@app.route('/potato_io/shots/count/46745', methods=['GET', 'POST'])
+def svs():
+    if 'logged_in' in session:
+        if session['level'] == '1':
+            session['level'] = '2'
+            return redirect(url_for('l3'))
+        else:
+            return redirect(request.referrer)
+    else:
+        flash('user must login first', 'danger')
+        return redirect(url_for(homepage))
 
 @app.route('/you_are_looking_for_nums', methods=['GET', 'POST'])
 def l3():
     if 'logged_in' in session:
-        if session['level'] >= '1':
+        if session['level'] >= '2':
             if request.method == 'POST':
-                if request.form['password'] == '':
-                    session['level'] = '2'
-                    return render_template('level3.html')
+                if request.form['password'] == '9152':
+                    session['level'] = '3'
+                    return redirect(url_for('l4'))
                 else:
                     flash('wrong password', 'danger')
-                    return redirect(url_for('l2'))
+                    return redirect(url_for('l3'))
             else:
-                return render_template('password_page.html')
+                return render_template('level3.html')
         else:
             return redirect(request.referrer)
     else:
         flash('User must login first', 'danger')
         return redirect(url_for('homepage'))
+
+
+@app.route('/level4', methods=['GET', 'POST'])
+def l4():
+    if 'logged_in' in session:
+        if session['level'] >= '3':
+            return render_template('level4.html')
+        else:
+            return redirect(request.referrer)
+    else:
+        flash('User must login first', 'danger')
+        return redirect(url_for('homepage'))
+
+
+@app.route('/route_5', methods=['GET', 'POST'])
+def logg5():
+    if 'logged_in' in session:
+        if session['level'] >= '3':
+            if request.method == 'POST':
+                if request.form['password'] == '':
+                    return redirect(url_for('l5'))
+                else:
+                    flash('wrong password','danger')
+                    return render_template('password_page.html')
+            else:
+                return render_template('password_page.html')
+        else:
+            return redirect(url_for('homepage'))
+    else:
+        flash('User must login first', 'danger')
+        return redirect(url_for('homepage'))
+
+
+@app.route('/turtle', methods=['GET', 'POST'])
+def l5():
+    if 'logged_in' in session:
+        if session['level'] >= '4':
+            session['level'] = '5'
+            return re
+
+
 
 
 @app.route('/logout')
